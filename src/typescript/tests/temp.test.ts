1c58c983329a3305ce5ccda164ca608c6d5528e1
Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>
2024-08-20 00:02:08 -0300

    docs: clean up
    Signed-off-by: Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>

--- src/typescript/tests/temp.test.ts
+++
@@ -1,9 +0,0 @@
-import { linearSearch } from '../code/linearSearch';
-
-test('linear_search finds the value in the array', () => {
-    const arr = [10, "hello", 12, 45.23, 111];
-    expect(linearSearch(arr, "hello")).toEqual(true);
-    expect(linearSearch(arr, 11)).toEqual(false);
-    expect(linearSearch(arr, "not_in_array")).toEqual(false);
-    expect(linearSearch(arr, 45.23)).toEqual(true);
-});
